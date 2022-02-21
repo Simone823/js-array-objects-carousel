e1637b599965f2f6952293a3d53c6e76fe8e50f4
Simone823 <simone.daglio823@outlook.it>
2022-02-21 16:15:04 +0100

    array oggetti items

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+// Array oggetti
+const arrayItems = [
+    {
+        immagine: "img/01.jpg",
+        titolo: "Svezia",
+        testo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+    },
+
+    {
+        immagine: "img/02.jpg",
+        titolo: "Svizzera",
+        testo: "Lorem ipsum",
+    },
+
+    {
+        immagine: "img/03.jpg",
+        titolo: "Gran Bretagna",
+        testo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    },
+
+    {
+        immagine: "img/04.jpg",
+        titolo: "Germania",
+        testo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+    },
+
+    {
+        immagine: "img/05.jpg",
+        titolo: "Paradise",
+        testo: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+    },
+]
+console.log(arrayItems);
+
+
+
+
+
 // Creo un'array contenente tutte le immagini
 const items = [
     'img/01.jpg',
